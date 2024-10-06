8dd9ddbb2f5d90e4098b41ee4ba61c76007ce9ea
inNN-Abner <abner.faculdade440@gmail.com>
2024-10-06 15:20:54 -0300

    Inserindo "IdentificationCard" na tela de perfil

--- src/screens/PerfilScreen/index.tsx
+++ src/screens/PerfilScreen/index.tsx
@@ -1,10 +1,17 @@
-import { GenericText, HeaderPage , Container  } from '../../components'
+import React from 'react'
+import { HeaderPage , Container, IdentificationCard, GenericText  } from '../../components'
 
 export const PerfilScreen = () => {
   return (
-    <Container align='center'>
+    <Container align='flex-start' style={{ flex: 1 }}>
+
       <HeaderPage />
-      <GenericText>Perfil</GenericText>
+      
+        <GenericText ftype='pageTitle' color='lightGray' ftSz='30' mgtop='30' mgleft='18'>Meu Perfil</GenericText>
+        <GenericText ftype='pageSubtitle' color='lightGray' ftSz='20' mgtop='5' mgleft='18'>Trocar o modelo do Header</GenericText>
+
+        <IdentificationCard></IdentificationCard>
+
     </Container>
   )
 }
